b2a5e93249fb61b9bdc0ae6e60d8f5b51c51f483
nottj <julian.nott@edu.htl-villach.at>
2023-12-17 19:56:13 +0100

    added Ü10

---
+++ uebung10_musicalbum/js/main.js
@@ -0,0 +1,14 @@
+/*
+Project: uebung08_cars
+Author:  nottj
+Date:    28.11.2023
+*/
+
+'use strict';
+
+const appName = 'Autos';
+document.getElementById('myTitle').innerText = appName;
+document.getElementById('myHeading1').innerText = appName;
+
+//inputs
+
